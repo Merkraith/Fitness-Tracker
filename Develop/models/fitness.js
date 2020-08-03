0f88f18c705ca21d669ff7ae4a8174454a58b7c5
Mark Heil <mark.heil1122@gmail.com>
2020-08-03 00:42:39 -0700

    fitness.js edits

--- Develop/models/fitness.js
+++ Develop/models/fitness.js
@@ -3,11 +3,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const fitnessSchema = new Schema({
-    name: {
-
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [{
+    type: {
+      type: String,
+      trim: true,
+      required: "Enter exercise type: resistance or cardio"
     },
-  }
-);
+    name: {
+      type: String,
+      trim: true,
+      required: "Exercise"
+    },
+    duration: {
+      type: Number,
+    },
+    weight: {
+      type: Number,
+    },
+    reps: {
+      type: Number,
+    },
+    sets: {
+      type: Number,
+    },
+    distance: {
+      type: Number,
+    },
+  ]
+})
 
 const Fitness = mongoose.model("fitness", fitnessSchema);
 
